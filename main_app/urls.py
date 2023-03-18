ea0a193a744ea00648e9625a44094b130d54a4d2
fatima20 <f6iim.ali@gmail.com>
2023-03-18 21:57:26 +0300

    test push

---
+++ main_app/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from . import views # dot(.) here mean root
+
+urlpatterns = [
+    # Routes in Express , URLs in Django
+    # dont add / here this mean root
+    path('', views.home, name='home') ,
+
+    # signup rout
+    path('accounts/signup/',views.signup , name='signup'),
+
+    # departments URL's
+    path('departments/', views.DepartmentsList.as_view(), name='departments_index'),
+    path('departments/<int:pk>',views.DepartmentsDetail.as_view(),name='departments_detail'),
+    path('departments/create', views.DepartmentsCreate.as_view(),name='departments_create'),
+    path('departments/<int:pk>/update', views.DepartmentsUpdate.as_view(),name='departments_update'),
+    path('departments/<int:pk>/delete/', views.DepartmentsDelete.as_view(), name='departments_delete'),
+
+    # doctorss URL's
+    path('doctors/', views.DoctorsList.as_view(), name='doctors_index'),
+    path('doctors/<int:pk>',views.DoctorsDetail.as_view(),name='doctors_detail'),
+    path('doctors/create', views.DoctorsCreate.as_view(),name='doctors_create'),
+    path('doctors/<int:pk>/update', views.DoctorsUpdate.as_view(),name='doctors_update'),
+    path('doctors/<int:pk>/delete/', views.DoctorsDelete.as_view(), name='doctors_delete'),
+]
---
+++ main_app/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render , redirect
+from .models import Department ,Doctor , Profile
+# this should be used to class based
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+# here no edit bcz it just return data from database - No change in DB
+from django.views.generic import ListView , DetailView
+# these 2 lines was imported to create form of signup
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+
+def home(request):
+    return render(request,'home.html')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # Make a 'user' form object with the data from the browser
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            # save user to DB
+            user = form.save()
+            # Log in the user automatically once they sign up
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid: Please Try Again!'
+    # If there's a bad post or get request
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
+
+# Departments CBV's
+class DepartmentsList(ListView):
+  model = Department
+
+class DepartmentsDetail(DetailView):
+  model = Department
+
+class DepartmentsCreate(CreateView):
+  model = Department
+  fields = '__all__'
+
+class DepartmentsUpdate(UpdateView):
+  model = Department
+  fields = '__all__'
+
+class DepartmentsDelete(DeleteView):
+  model = Department
+  success_url = '/departments/'
+
+
+# Doctor CBV's
+class DoctorsList(ListView):
+  model = Doctor
+
+class DoctorsDetail(DetailView):
+  model = Doctor
+
+class DoctorsCreate(CreateView):
+  model = Doctor
+  # profile = Profile.objects.filter()
+  fields = '__all__'
+
+class DoctorsUpdate(UpdateView):
+  model = Doctor
+  fields = '__all__'
+
+class DoctorsDelete(DeleteView):
+  model = Doctor
+  success_url = '/doctors/'
